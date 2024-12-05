24851c4bd382afd3c1951610962c319a0ab42f4a
Kyle Helstowski <144864786+Kwibs@users.noreply.github.com>
2024-12-05 15:28:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function revealGif() {
+    const gif = document.getElementById('gif');
+    // Toggle the visibility of the GIF
+    gif.style.display = gif.style.display === 'block' ? 'none' : 'block';
+  }
+  
